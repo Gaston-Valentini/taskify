ba306831d27c273e332b75aef19434e9c567d9b8
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-11-21 19:06:35 +0100

    Add: User entity added.

---
+++ server/src/entities/User.ts
@@ -0,0 +1,29 @@
+import {
+    BaseEntity,
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    username!: string;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    password!: string;
+
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+}
